b375bdc90dcbc34130ea9591bb48a7fc3bdf0dea
hajinlee93 <hajin93040@gmail.com>
2024-05-02 16:36:15 +0900

    push

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,60 +1,67 @@
 // "use client";
 // import { useEffect, useState } from "react"
 
-export const metadata = {
-    title: 'Home'                     
-}
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+import Link from "next/link";
 
-async function getMovies(){
-    return fetch(URL).then(response => response.json());
+export const metadata = {
+  title: "Home",
+};
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return fetch(API_URL).then((response) => response.json());
 }
 
 /**
  * 
  * 
     async function getMovies(){
-        return fetch(URL).then(response => response.json());
+        return fetch(API_URL).then(response => response.json());
     }
  
     위에 있는 함수와  아래의 함수는 같다
 
      async function getMovies(){
-        const response = await fetch(URL);
+        const response = await fetch(API_URL);
         const json = await response.json();
         return json;
     }
  * 
  */
 
+export default async function HomePage() {
+  // const [isLoading,setisLoading] = useState(true);
+  // const [movies,setMovies] = useState([]);
 
-export default async function HomePage(){
-    // const [isLoading,setisLoading] = useState(true);
-    // const [movies,setMovies] = useState([]);
+  // const getMovies = async () => {
+  //     const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
+  //     const json = await response.json();
+  //     setMovies(json);
+  //     setisLoading(false);
+  // }
 
-    // const getMovies = async () => {
-    //     const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");
-    //     const json = await response.json();
-    //     setMovies(json);
-    //     setisLoading(false);
-    // }
+  // useEffect(()=>{
+  //     getMovies();
+  // },[])
 
-    // useEffect(()=>{
-    //     getMovies();
-    // },[])
+  /**
+   *  const [movies,setMovies] = useState();
+   *
+   * 이렇게 받아서 쓴다. 즉, movies가 state 그 자체인 것이고, setMovies 은 이 state 를 변경할 때 사용하는 함수인 것이다.
+   * */
 
-    /**
-     *  const [movies,setMovies] = useState();
-     * 
-     * 이렇게 받아서 쓴다. 즉, movies가 state 그 자체인 것이고, setMovies 은 이 state 를 변경할 때 사용하는 함수인 것이다.
-     * */ 
+  const movies = await getMovies();
 
-    const movies = await getMovies();
-
-	return (
-        <div>
-            {/* {isLoading ? "Loading..." : JSON.stringify(movies)} */}
-            {JSON.stringify(movies)}
-        </div>
-    );
+  return (
+    <div>
+      {/* {isLoading ? "Loading..." L: JSON.stringify(movies)} */}
+      {/* {JSON.stringify(movies)} */}
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,4 +1,80 @@
-export default function MovieDetail({params:{id},}:{params :{id :string}}){ // params 내부에 있는 id 를 가지고 올꺼고 그 params의 id는 문자열로 되어있다는 뜻 
-    return <h1>Movie {id}</h1>;
+import { resolve } from "path";
+import { API_URL } from "../../../(home)/page";
+import MovieVideos from "../../../../components/movie-videos";
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+
+export const metadata = {
+  title: "Movies",
+};
+
+// async function getMovies(id: string) {
+//   console.log(`Fetching moives : ${Date.now()}`);
+//   await new Promise((resolve) => setTimeout(resolve, 5000));
+//   const response = await fetch(`${API_URL}/${id}`);
+//   return response.json();
+// }
+
+// async function getVideos(id: string) {
+//   console.log(`Fetching videos : ${Date.now()}`);
+//   await new Promise((resolve) => setTimeout(resolve, 5000));
+//   const response = await fetch(`${API_URL}/${id}`);
+//   return response.json();
+// }
+
+export default async function MovieDetail({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  // params 내부에 있는 id 를 가지고 올꺼고 그 params의 id는 문자열로 되어있다는 뜻
+
+  //   console.log('Start Fetching');
+  //   const movie = await getMovies(id);
+  //   const video = await getVideos(id);
+  //   const [movie, video] = await Promise.all([getMovies(id), getVideos(id)]);
+  //   console.log('end Fetching');
+
+  /**
+ * 
+  console.log('Start Fetching');
+  const movie = await getMovies(id);
+  const video = await getVideos(id);
+  console.log('end Fetching');
+ * 결과값
+Start Fetching
+Fetching moives : 1714625542501
+Fetching videos : 1714625547506
+end Fetching
+
+===============
+
+const [movie,video] = await Promise.all([getMovies(id),getVideos(id)])
+결과값
+Start Fetching
+Fetching moives : 1714625817827
+Fetching videos : 1714625817828
+end Fetching
+
+========================
+  const [movie,video] = await Promise.all([getMovies(id),getVideos(id)]);
+  병렬적으로 실행은 되지만 이런 방법도 아직 문제는 남아있음 
+  -> movie와 video  두가지 data가 모두 왔을 떄 보여진다
+
+ * 
+ * */
+
+  return (
+    <div>
+        <h3>Movie detail page</h3>
+      <Suspense fallback={<h1>Loading movies info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movies videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
 
+// Promise.all -> fetch 하는 API가 2개 이상인경우 데이터를 하나 가져오로 다음꺼 가지고 오는게 아닌 같이 await하기 위해 사용
