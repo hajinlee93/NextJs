90e9b3d4ac8967588e3ee6767c9e161364ca5d7d
hajinlee93 <hajin93040@gmail.com>
2024-05-03 16:18:13 +0900

    push

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 };
 export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export async function getMovies() {
+async function getMovies() {
   //   await new Promise((resolve) => setTimeout(resolve, 1000));
   return fetch(API_URL).then((response) => response.json());
 }
